0d420230bd5c47f7d208cfb68324bf4800976558
MUÑIZ SANCHEZ LUIS MANUEL <nsskn.zaap@gmail.com>
2026-06-14 14:01:55 -0600

    Add files via upload

---
+++ funciones.py
@@ -0,0 +1,34 @@
+
+"""
+  Una función es un conjunto de instrucciones agrupadas bajo un nombre en particular como un programa mas pequeño que cumple una funcion especifica. La funcion se puede reutulizar con el simple hecho de invocarla es decir mandarla llamar 
+
+  Sintaxis:
+
+   def nombredeMifuncion(parametros):
+      bloque o conjunto de instrucciones
+
+   nombredeMifuncion(parametros)
+
+   Las funciones pueden ser de 4 tipos
+  
+    Funciones de tipo "Procedimiento" 
+   1.- Funcion que no recibe parametros y no regresa valor
+   3.- Funcion que recibe parametros y no regresa valor
+    
+    Funciones de tipo "Funcion"
+   2.- Funcion que no recibe parametros y regresa valor
+   4.- Funcion que recibe parametros y regresa valor
+
+"""
+#1.- Funcion que no recibe parametros y no regresa valor
+
+
+ #3.- Funcion que recibe parametros y no regresa valor 
+
+
+ #2.- Funcion que no recibe parametros y regresa valor
+
+ #4.- Funcion que recibe parametros y regresa valor
+
+#Invocar las funciones
+
